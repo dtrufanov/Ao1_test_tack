ed91cb308f4b40b17e0f11c9fec5ed74f36dbc41
dtrufanov <dtrufanov@moysklad.ru>
2019-07-19 17:09:33 +0300

    generic for product identification, simple test

--- ao1test/src/main/java/trufanov/ao1/data/ResultHolder.java
+++ ao1test/src/main/java/trufanov/ao1/data/ResultHolder.java
@@ -1,20 +1,22 @@
 package trufanov.ao1.data;
 
 import java.util.*;
-import java.util.function.Consumer;
 import java.util.function.Function;
 
-public class ResultHolder {
-    private double maxPrice;
+public class ResultHolder<T> {
     private LinkedList<Product> products = new LinkedList<>();
+    private Map<T, ProductMetrics> metrics = new HashMap<>();
+    private double maxPrice;
     private int maxSize;
     private int maxSameSize;
+    private Function<Product, T> identification;
 
-    private Map<Long, ProductMetrics> metrics = new HashMap<>();
+    private static final double NO_PRICE = Double.NEGATIVE_INFINITY;
 
-    public ResultHolder(int maxSize, int maxSameSize) {
+    public ResultHolder(int maxSize, int maxSameSize, Function<Product, T> identification) {
         this.maxSize = maxSize;
         this.maxSameSize = maxSameSize;
+        this.identification = identification;
     }
 
     public List<Product> getProducts() {
@@ -22,16 +24,31 @@ public class ResultHolder {
     }
 
     public void add(Product product) {
-        if (products.size() < maxSize && getQuantity(product.getId()) < maxSameSize) {
-            products.add(product);
-            Collections.sort(products);
-            updateMetrics(product);
+        ProductMetrics productMetrics = getMetrics(product);
+        boolean noNeedReplace = products.size() < maxSize && productMetrics.getQuantity() < maxSameSize;
+        if (noNeedReplace) {
+            addProduct(product);
             return;
         }
-        if (product.getPrice() > maxPrice) {
+        if (product.getPrice() > maxPrice || productMetrics.getQuantity() >= maxSameSize && product.getPrice() > productMetrics.getMaxPrice()) {
             return;
         }
-        Product productToRemove = findExpensiveProduct(product);
+        replaceProduct(productMetrics, product);
+    }
+
+    private ProductMetrics getMetrics(Product product) {
+        ProductMetrics productMetrics = metrics.get(identification.apply(product));
+        return productMetrics != null ? productMetrics : new ProductMetrics();
+    }
+
+    private void addProduct(Product product) {
+        products.add(product);
+        Collections.sort(products);
+        updateMetrics(product);
+    }
+
+    private void replaceProduct(ProductMetrics productMetrics, Product product) {
+        Product productToRemove = findExpensiveProduct(productMetrics, product);
         if (productToRemove != null) {
             products.remove(productToRemove);
             products.add(product);
@@ -43,40 +60,38 @@ public class ResultHolder {
         }
     }
 
-    private long getQuantity(long id) {
-        ProductMetrics productMetrics = metrics.get(id);
-        return productMetrics != null ? productMetrics.getQuantity() : 0;
-    }
-
-    private Product findExpensiveProduct(Product productToAdd) {
-        ProductMetrics productMetrics = metrics.get(productToAdd.getId());
-        if (productMetrics != null) {
-            if (productMetrics.getQuantity() < maxSameSize) {
-                return products.getLast();
-            }
-            if (productToAdd.getPrice() < productMetrics.getMaxPrice()){
-                return products.stream().filter(p -> p.getId() == productToAdd.getId()).max(Comparator.comparingDouble(Product::getPrice)).get();//todo get
-            }
+    private Product findExpensiveProduct(ProductMetrics productMetrics, Product productToAdd) {
+        if (productMetrics.getQuantity() < maxSameSize) {
+            return products.getLast();
+        }
+        if (productToAdd.getPrice() < productMetrics.getMaxPrice()){
+            return products.stream().filter(p -> identification.apply(p).equals(identification.apply(productToAdd))).max(Comparator.comparingDouble(Product::getPrice)).get();//todo get
         }
         return null;
     }
 
     private void updateMetrics(Product product) {
-        ProductMetrics productMetrics = metrics.get(product.getId());
+        T id = identification.apply(product);
+        ProductMetrics productMetrics = metrics.get(id);
         if (productMetrics == null) {
-            metrics.put(product.getId(), new ProductMetrics(product));
+            metrics.put(id, new ProductMetrics(product));
         } else {
-            productMetrics.setQuantity(products.stream().filter(p -> p.getId() == product.getId()).count());
-            OptionalDouble optionMax = products.stream().filter(p -> p.getId() == product.getId()).mapToDouble(Product::getPrice).max();
-            productMetrics.setMaxPrice(optionMax.isPresent() ? optionMax.getAsDouble() : Double.NaN);
+            productMetrics.setQuantity(products.stream().filter(p -> identification.apply(p).equals(identification.apply(product))).count());
+            OptionalDouble optionMax = products.stream().filter(p -> identification.apply(p).equals(identification.apply(product))).mapToDouble(Product::getPrice).max();
+            productMetrics.setMaxPrice(optionMax.isPresent() ? optionMax.getAsDouble() : NO_PRICE);
         }
-        maxPrice = products.stream().mapToDouble(Product::getPrice).max().getAsDouble();
+        maxPrice = products.stream().mapToDouble(Product::getPrice).max().orElse(NO_PRICE);
     }
 
 
     private class ProductMetrics {
         long quantity;
         double maxPrice;
+
+        ProductMetrics() {
+            this.quantity = 0L;
+            this.maxPrice = NO_PRICE;
+        }
 
         ProductMetrics(Product product) {
             quantity = 1L;
--- ao1test/src/test/java/trufanov/ao1/test/ResultHolderTest.java
+++ ao1test/src/test/java/trufanov/ao1/test/ResultHolderTest.java
@@ -5,16 +5,33 @@ import trufanov.ao1.data.Product;
 import trufanov.ao1.data.ResultHolder;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
 
 public class ResultHolderTest {
     @Test
     public void test() throws IOException {
-        List<String> lines = TestUtils.readFile("input/sample.csv");
-        ResultHolder resultHolder = new ResultHolder(10, 3);
+        baseTest("test_1", 1, 1);
+        baseTest("test_1", 2, 1);
+        baseTest("test_1", 2, 2);
+        baseTest("test_1", 4, 2);
+        baseTest("test_1", 6, 1);
+        baseTest("test_1", 6, 3);
+    }
+
+    public void baseTest(String filename, int maxSize, int maxSameSize) throws IOException {
+        List<String> lines = TestUtils.readFile("input/" + filename + ".csv");
+//        Collections.shuffle(lines);
+        ResultHolder resultHolder = new ResultHolder<>(maxSize, maxSameSize, Product::getId);
         for (String line : lines) {
             resultHolder.add(Product.parseProduct(line));
         }
-        System.out.println(resultHolder.getProducts());
+        List<String> expected = TestUtils.readFile("input/" + filename + "_check_" + maxSize + "_" + maxSameSize + ".csv");
+        List<Product> result = resultHolder.getProducts();
+        assertEquals("Unexpected result size", expected.size(), result.size());
+        assertEquals(expected, result.stream().map(Product::getLine).collect(Collectors.toList()));
     }
 }
